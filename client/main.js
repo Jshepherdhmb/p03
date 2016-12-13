804bd6ba82b2ab748dd819bf94956221565731a3
Jordan Shepherd <jshepherd11@mail.csuchico.edu>
2016-12-12 20:53:08 -0800

    Organized Code

--- client/main.js
+++ client/main.js
@@ -2,75 +2,67 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Session } from 'meteor/session'
 import './main.html';
-PlayersList = new Mongo.Collection('players');
-UsersText = new Mongo.Collection('Text');
-AnimeText = new Mongo.Collection('Anime');
-CartoonText = new Mongo.Collection('Cartoons');
-FPSText = new Mongo.Collection('FPS');
-RTSText = new Mongo.Collection('RTS');
-BGText = new Mongo.Collection('Board Games');
-GDText = new Mongo.Collection('Game Dev');
-MOBAText = new Mongo.Collection('MOBA');
-FightText = new Mongo.Collection('Fighting Games');
-DrawText = new Mongo.Collection('Drawing');
-LitText = new Mongo.Collection('Literature');
-SportsText = new Mongo.Collection('Sports');
-HikeText = new Mongo.Collection('Hiking');
-Session.set('m','LitText');
-var i = 0;
+	PlayersList = new Mongo.Collection('players');
+	UsersText = new Mongo.Collection('Text');
+	AnimeText = new Mongo.Collection('Anime');
+	CartoonText = new Mongo.Collection('Cartoons');
+	FPSText = new Mongo.Collection('FPS');
+	RTSText = new Mongo.Collection('RTS');
+	BGText = new Mongo.Collection('Board Games');
+	GDText = new Mongo.Collection('Game Dev');
+	MOBAText = new Mongo.Collection('MOBA');
+	FightText = new Mongo.Collection('Fighting Games');
+	DrawText = new Mongo.Collection('Drawing');
+	LitText = new Mongo.Collection('Literature');
+	SportsText = new Mongo.Collection('Sports');
+	HikeText = new Mongo.Collection('Hiking');
+	var i = 0;
 
 
 if(Meteor.isClient){
 
- Template.database.helpers({
-tasks: function(){
-return UsersText.find({}, {sort: {createdAt: -1}});
-}
-});
-
 Template.Chats.helpers({
 //Tried to Create a session to change a variable that would specify a database so I wouldn't have to create the same function for every data however I couldn't update the client javascript how I wanted to so I chose to use css instead
-LitTasks: function(){
-return LitText.find({}, {sort: {createdAt: -1}});
-},
-AnimeTasks: function(){
-return AnimeText.find({}, {sort: {createdAt: -1}});
-},
-CartoonTasks: function(){
-return CartoonText.find({}, {sort: {createdAt: -1}});
-},
-FPSTasks: function(){
-return FPSText.find({}, {sort: {createdAt: -1}});
-},
-RTSTasks: function(){
-return RTSText.find({}, {sort: {createdAt: -1}});
-},
-BGTasks: function(){
-return BGText.find({}, {sort: {createdAt: -1}});
-},
-GDTasks: function(){
-return GDText.find({}, {sort: {createdAt: -1}});
-},
-MOBATasks: function(){
-return MOBAText.find({}, {sort: {createdAt: -1}});
-},
-FightTasks: function(){
-return FightText.find({}, {sort: {createdAt: -1}});
-},
-SportsTasks: function(){
-return SportsText.find({}, {sort: {createdAt: -1}});
-},
-HikeTasks: function(){
-return HikeText.find({}, {sort: {createdAt: -1}});
-},
-DrawTasks: function(){
-return DrawText.find({}, {sort: {createdAt: -1}});
-}
+	LitTasks: function(){
+		return LitText.find({}, {sort: {createdAt: -1}});
+	},
+	AnimeTasks: function(){
+		return AnimeText.find({}, {sort: {createdAt: -1}});
+	},
+	CartoonTasks: function(){
+		return CartoonText.find({}, {sort: {createdAt: -1}});
+	},
+	FPSTasks: function(){
+		return FPSText.find({}, {sort: {createdAt: -1}});
+	},
+	RTSTasks: function(){
+		return RTSText.find({}, {sort: {createdAt: -1}});
+	},
+	BGTasks: function(){
+		return BGText.find({}, {sort: {createdAt: -1}});
+	},
+	GDTasks: function(){
+		return GDText.find({}, {sort: {createdAt: -1}});
+	},
+	MOBATasks: function(){
+		return MOBAText.find({}, {sort: {createdAt: -1}});
+	},
+	FightTasks: function(){
+		return FightText.find({}, {sort: {createdAt: -1}});
+	},
+	SportsTasks: function(){
+		return SportsText.find({}, {sort: {createdAt: -1}});
+	},
+	HikeTasks: function(){
+		return HikeText.find({}, {sort: {createdAt: -1}});
+	},
+	DrawTasks: function(){
+		return DrawText.find({}, {sort: {createdAt: -1}});
+	}
 });
 Template.Chats.events({
 
  'click submit': function(){	
-Meteor.call('HW');
 }
 });
 }//Dynamic Templates from http://stackoverflow.com/questions/25618125/meteor-show-and-hide-templates
--- server/main.js
+++ server/main.js
@@ -1,45 +1,43 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-PlayersList = new Mongo.Collection('players');
-UsersText = new Mongo.Collection('Text');
-AnimeText = new Mongo.Collection('Anime');
-AnimeText.insert({text: "Instantiate"});
-CartoonText = new Mongo.Collection('Cartoons');
-CartoonText.insert({text: "Instantiate"});
-FPSText = new Mongo.Collection('FPS');
-FPSText.insert({text: "Instantiate"});
-RTSText = new Mongo.Collection('RTS');
-RTSText.insert({text: "Instantiate"});
-BGText = new Mongo.Collection('Board Games');
-BGText.insert({text: "Instantiate"});
-GDText = new Mongo.Collection('Game Dev');
-GDText.insert({text: "Instantiate"});
-MOBAText = new Mongo.Collection('MOBA');
-MOBAText.insert({text: "Instantiate"});
-FightText = new Mongo.Collection('Fighting Games');
-FightText.insert({text: "Instantiate"});
-DrawText = new Mongo.Collection('Drawing');
-DrawText.insert({text: "Instantiate"});
-LitText = new Mongo.Collection('Literature');
-LitText.insert({text: "Instantiate"});
-SportsText = new Mongo.Collection('Sports');
-SportsText.insert({text: "Instantiate"});
-HikeText = new Mongo.Collection('Hiking');
-HikeText.insert({text: "Instantiate"});
-Meteor.methods({
-'HW': function(){
-console.log("HW");
-},
-'deleteDocs': function(Name, filter = {}) {
-	if(Name == 'Lit')
-   		 return LitText.remove(filter);
-	else if(Name = 'BG')
-		 return BG.remove(filter);
-  }
+	PlayersList = new Mongo.Collection('players');
+	UsersText = new Mongo.Collection('Text');
+	AnimeText = new Mongo.Collection('Anime');
+	AnimeText.insert({text: "Instantiate"});
+	CartoonText = new Mongo.Collection('Cartoons');
+	CartoonText.insert({text: "Instantiate"});
+	FPSText = new Mongo.Collection('FPS');
+	FPSText.insert({text: "Instantiate"});
+	RTSText = new Mongo.Collection('RTS');
+	RTSText.insert({text: "Instantiate"});
+	BGText = new Mongo.Collection('Board Games');
+	BGText.insert({text: "Instantiate"});
+	GDText = new Mongo.Collection('Game Dev');
+	GDText.insert({text: "Instantiate"});
+	MOBAText = new Mongo.Collection('MOBA');
+	MOBAText.insert({text: "Instantiate"});
+	FightText = new Mongo.Collection('Fighting Games');
+	FightText.insert({text: "Instantiate"});
+	DrawText = new Mongo.Collection('Drawing');
+	DrawText.insert({text: "Instantiate"});
+	LitText = new Mongo.Collection('Literature');
+	LitText.insert({text: "Instantiate"});
+	SportsText = new Mongo.Collection('Sports');
+	SportsText.insert({text: "Instantiate"});
+	HikeText = new Mongo.Collection('Hiking');
+	HikeText.insert({text: "Instantiate"});
+	Meteor.methods({
+		//courtesy of https://forums.meteor.com/t/deleting-all-documents-from-a-collection-from-the-web-console/32230/2
+		'deleteDocs': function(Name, filter = {}) {
+			if(Name == 'Lit')
+   				 return LitText.remove(filter);
+			else if(Name = 'BG')
+				 return BG.remove(filter);
+ 		 }
 });
 
-//courtesy of https://forums.meteor.com/t/deleting-all-documents-from-a-collection-from-the-web-console/32230/2
+
   // code to run on server at startup
 });
 
